b86ebc6394705198244e15653693e0783b28499a
fred144 <fgarcia4@terpmail.umd.edu>
2024-02-05 17:40:02 -0500

    gas contours

---
+++ science/sbatch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -J gas_renderer
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=3999
+#SBATCH -t 24:00:00
+
+#. ~/.bashrc
+. ~/.profile
+
+# designed to be submitted/ ran in the script directory
+export LANG=en_US
+module purge
+module load python
+module load gcc
+module load openmpi
+# activate py environment
+source ~/scratch/master/bin/activate
+ 
+SCRIPT="/scratch/zt1/project/ricotti-prj/user/fgarcia4/tiramisu/science/gas_density.py"
+DIR="/scratch/zt1/project/ricotti-prj/user/ricotti/GC-Fred/CC-Fiducial"
+NICKNAME="VSFE Density Contour"
+# also tried
+# cd /scratch/zt1/project/ricotti-prj/user/fgarcia4/globclustevo/visuals/
+# SCRIPT = "./low-sfe.py"
+# number of cores specified above
+mpirun python3 $SCRIPT $DIR $NICKNAME 304 375 1  2>&1 
