339426181a7adfea13734977111b8ad794afec90
fred144 <fgarcia4@terpmail.umd.edu>
2024-02-20 15:40:10 -0500

    gas properties

--- movie_wzrd/sbatch_gasprop_70sfe.sh
+++ movie_wzrd/sbatch_gasprop_70sfe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J 35sfegasProperties
 #SBATCH --ntasks=1
-#SBATCH -t 48:00:00
+#SBATCH -t 72:00:00
 #SBATCH --mem-per-cpu=8192
 #SBATCH --mail-user=g.fred@columbia.edu
 #SBATCH --mail-type=ALL
